f57a6da2b8262f4934b25a3c1ce2bd7df20c3bdf
Rushabh Prajapati <rushabhprajapati20@gmail.com>
2025-11-03 01:42:42 -0500

    - Update API layer by adding a safeApiCall mechanism to centralize error handling

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.rushdroid.benedictmovies"
-        minSdk = 24
+        minSdk = 27
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
